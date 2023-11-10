eb63315f6d9e81daf27953713791a007a7b5ba2f
theri <therigasmazurets@gmail.com>
2023-11-10 20:42:33 +0200

    Created

---
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.criminalintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.criminalintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+    }
+}
